8a0fce17f5ba685a4ab5aa5c02b81533e81751d1
Jorie286 <mcdermott.286@osu.edu>
2025-12-04 15:59:15 -0500

    Update repo with orbit class and plotting files.
    Wrote the start of the N-body simulator in the orbit classes file, some de-bugging still needs to be done on this.
    
    The orbit test file contains all the code necessary for plotting the orbits of the bodies in both cartisian and polar coordinates.

--- orbit_classes.py
+++ orbit_classes.py
@@ -2,19 +2,54 @@
 
 # import the necessary packages
 import numpy as np
-from scipy.integrate import solve_ivp
-
-# NEED TO MODIFY THE DY_DT AND SOLVE_ODE FUCNTIONS FOR THE PROBLEMS I AM SOLVING!!!
 
 
-class three_body_orbit:
+class N_body_orbit:
     """
     Equations of motion for a three body gravitational system with equal masses.
     """
-    def __init__(self, m=1, G=1): # initialize the necessary constants
-        self.m = m
-        self.G = G
+    def __init__(self, ang_mom, m, n=-1, k=1, mu=1):
+        self.ang_mom = ang_mom
+        self.m = m # mass of a body in the system (assume they are all the same to simplify the problem)
+        self.n = n
+        self.k = k
+        self.mu = mu
 
+    def U(self, r):
+        """Potential energy of the form U = kr^n."""
+        U_tot = 0
+        for i in range(len(r)-1):
+            U_i = self.k * np.abs(r[i] - r[i+1])**self.n
+            U_tot += U_i
+        return U_tot
+
+    # get the central and effective energy
+    def Ucf(self, r):
+        """Centrifugal potential energy"""
+        return self.ang_mom**2 / (2. * self.mu * (sum([self.m * r[i] for i in range(len(r)-1)])/(self.m**(len(r))))**2)
+
+    def Ueff(self, r):
+        """Effective potential energy"""
+        return self.U(r) + self.Ucf(r)
+
+    def U_deriv(self, r):
+        """dU/dr"""
+        U_deriv_tot = 0
+        for i in range(len(r)-1):
+            U_deriv_i = self.n * self.k * (r[i] - r[i+1]) * np.abs(r[i] - r[i+1])**(self.n - 1)
+            U_deriv_tot += U_deriv_i
+        return U_deriv_tot
+
+    # get the derivative of the central and effective energy
+    def Ucf_deriv(self, r):
+        """dU_cf/dr"""
+        return -2. * self.ang_mom**2 / (2. * self.mu * (sum([self.m * r[i] for i in range(len(r)-1)])/(self.m**(len(r))))**3)
+
+    def Ueff_deriv(self, r):
+        """dU_eff/dr"""
+        return self.U_deriv(r) + self.Ucf_deriv(r)
+
+    # the differential equation
     def dy_dt(self, t, y):
         """
         This function returns the right-hand side of the diffeq:
@@ -28,97 +63,38 @@ class three_body_orbit:
             3-component vector with y[0] = r(t), y[1] = dr/dt, y[2] = dphi/dt
 
         """
-        return [ y[1],
-                -1./self.mu * self.Ueff_deriv(y[0]),
-                self.ang_mom / (self.mu * y[0]**2) ]
+        y=np.array(y)
+        return [ y[:,1],
+                -1./self.mu * self.Ueff_deriv(y[:,0]),
+                self.ang_mom / (self.mu * y[:,0]**2) ]
 
-    def solve_ode(self, t_pts, r_0, r_dot_0, phi_0,
-                  abserr=1.0e-8, relerr=1.0e-8):
+    # integrate over the differential equation and return the final results
+    def solve_leapfrog(self, t_pts, delta_t, r_0, r_dot_0, phi_0):
         """
-        Solve the ODE given initial conditions.
-        Specify smaller abserr and relerr to get more precision.
+        Solve ODE using leapfrog method.
         """
-        y = [r_0, r_dot_0, phi_0]
-        solution = solve_ivp(self.dy_dt, (t_pts[0], t_pts[-1]),
-                             y, t_eval=t_pts,
-                             atol=abserr, rtol=relerr)
-        r, r_dot, phi = solution.y
+        # get the intial conditions and add them to the arrays that will be output
+        r = np.zeros((len(r_0), len(t_pts)))
+        r_dot = np.zeros((len(r_0), len(t_pts)))
+        r_dot_half = np.zeros((len(r_0), len(t_pts)))
+        phi = np.zeros((len(r_0), len(t_pts)))
+        r[:,0] = r_0
+        r_dot[:,0] = r_dot_0
+        phi[:,0] = phi_0
+
+        # step through the points and get the values
+        for i in range(len(t_pts)-1):
+            y = np.array([r[:,i], r_dot[:, i], phi[:, i]]).T
+            r_dot_half[:, i] = r_dot[:, i] + self.dy_dt(t_pts[i], y)[1] * (delta_t/2)
+            r[:, i+1] = r[:, i] + r_dot_half[:, i] * delta_t
+            phi_dot_half = self.ang_mom / (self.mu * r[:, i]**2)
+            phi[:, i+1] = phi[:, i] + phi_dot_half * delta_t
+
+            yplus = np.array([r[:, i+1], r_dot[:, i+1], phi[:, i+1]]).T
+            r_dot[:, i+1] = r_dot_half[:, i] + self.dy_dt(t_pts[i], yplus)[1] * (delta_t/2)
         return r, r_dot, phi
 
-
-class four_body_orbit:
-    """
-    Equations of motion for a four body gravitational system with equal masses.
-    """
-    def __init__(self, m=1, G=1): # initialize the necessary constants
-        self.m = m
-        self.G = G
-
-    def dy_dt(self, t, y):
-        """
-        This function returns the right-hand side of the diffeq:
-        [dr/dt d^2r/dt^2 dphi/dt]
-
-        Parameters
-        ----------
-        t : float
-            time
-        y : float
-            3-component vector with y[0] = r(t), y[1] = dr/dt, y[2] = dphi/dt
-
-        """
-        return [ y[1],
-                -1./self.mu * self.Ueff_deriv(y[0]),
-                self.ang_mom / (self.mu * y[0]**2) ]
-
-    def solve_ode(self, t_pts, r_0, r_dot_0, phi_0,
-                  abserr=1.0e-8, relerr=1.0e-8):
-        """
-        Solve the ODE given initial conditions.
-        Specify smaller abserr and relerr to get more precision.
-        """
-        y = [r_0, r_dot_0, phi_0]
-        solution = solve_ivp(self.dy_dt, (t_pts[0], t_pts[-1]),
-                             y, t_eval=t_pts,
-                             atol=abserr, rtol=relerr)
-        r, r_dot, phi = solution.y
-        return r, r_dot, phi
-
-
-class five_body_orbit:
-    """
-    Equations of motion for a five body gravitational system with equal masses.
-    """
-    def __init__(self, m=1, G=1): # initialize the necessary constants
-        self.m = m
-        self.G = G
-
-    def dy_dt(self, t, y):
-        """
-        This function returns the right-hand side of the diffeq:
-        [dr/dt d^2r/dt^2 dphi/dt]
-
-        Parameters
-        ----------
-        t : float
-            time
-        y : float
-            3-component vector with y[0] = r(t), y[1] = dr/dt, y[2] = dphi/dt
-
-        """
-        return [ y[1],
-                -1./self.mu * self.Ueff_deriv(y[0]),
-                self.ang_mom / (self.mu * y[0]**2) ]
-
-    def solve_ode(self, t_pts, r_0, r_dot_0, phi_0,
-                  abserr=1.0e-8, relerr=1.0e-8):
-        """
-        Solve the ODE given initial conditions.
-        Specify smaller abserr and relerr to get more precision.
-        """
-        y = [r_0, r_dot_0, phi_0]
-        solution = solve_ivp(self.dy_dt, (t_pts[0], t_pts[-1]),
-                             y, t_eval=t_pts,
-                             atol=abserr, rtol=relerr)
-        r, r_dot, phi = solution.y
-        return r, r_dot, phi
+    # get the energy of the system
+    def energy(self, t_pts, r, r_dot):
+        """Evaluate the energy as a function of time"""
+        return (self.mu/2.) * r_dot**2 + self.Ueff(r)
---
+++ orbit_test.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from orbit_classes_old import N_body_orbit
+
+n = -1
+k = -3.
+ang_mom = 1.
+m = 1.
+o1 = N_body_orbit(ang_mom, m, n=n, k=k, mu=0.5)
+
+# Plotting time
+t_start = 0.
+t_end = 10.
+delta_t = 0.01
+
+t_pts = np.arange(t_start, t_end+delta_t, delta_t)
+
+# Initial conditions
+r_0 = np.array([1.001, -1.002])
+r_dot_0 = np.array([1.0, 1.0])
+
+deg_2_rad = np.pi/180
+phi_0 = np.array([0.001, 0.001])
+r_pts_l, r_dot_pts_l, phi_pts_l = o1.solve_leapfrog(t_pts, delta_t, r_0, r_dot_0, phi_0)
+
+c = o1.ang_mom**2 / (np.abs(o1.k) * o1.mu)
+epsilon = c / r_0 - 1.
+energy_0 = o1.mu/2. * r_dot_0**2 + o1.Ueff(r_0)
+
+# print(f'energy = {energy_0:.2f}')
+# print(f'eccentricity = {epsilon:.2f}')
+
+fig_4 = plt.figure(figsize=(8,8))
+
+overall_title = 'Gravitational orbit:  ' + \
+                rf' $n = {o1.n},$' + \
+                rf' $k = {o1.k:.1f},$' + \
+                rf' $l = {o1.ang_mom:.1f},$' + \
+                rf' $r_0 = {r_0},$' + \
+                rf' $\dot r_0 = {r_dot_0},$' + \
+                rf' $\phi_0 = {phi_0}$' + \
+                '\n'     # \n means a new line (adds some space here)
+fig_4.suptitle(overall_title, va='baseline')
+
+ax_4c = fig_4.add_subplot(1,1,1)
+ax_4c.plot(r_pts_l[0,:]*np.cos(phi_pts_l[0,:]), r_pts_l[0,:]*np.sin(phi_pts_l[0,:]), color='blue', label = "Leapfrog")
+ax_4c.plot(r_pts_l[1,:]*np.cos(phi_pts_l[1,:]), r_pts_l[1,:]*np.sin(phi_pts_l[1,:]), color='red', label = "Leapfrog")
+#ax_4c.plot(r_pts_l[2,:]*np.cos(phi_pts_l[2,:]), r_pts_l[2,:]*np.sin(phi_pts_l[2,:]), color='green', label = "Leapfrog")
+#ax_4c.plot(r_pts_l[3,:]*np.cos(phi_pts_l[3,:]), r_pts_l[3,:]*np.sin(phi_pts_l[3,:]), color='green', label = "Leapfrog")
+ax_4c.set_xlabel(r'$x$')
+ax_4c.set_ylabel(r'$y$')
+ax_4c.set_aspect(1)
+ax_4c.set_title('Cartesian plot')
+ax_4c.set_xlim(-10,10)
+ax_4c.set_ylim(-10,10)
+
+fig_4.tight_layout()
+
+fig_4.savefig("Orbits_plots.png", bbox_inches = "tight")
+
+
+fig_4 = plt.figure(figsize=(8,8))
+
+overall_title = 'Gravitational orbit:  ' + \
+                rf' $n = {o1.n},$' + \
+                rf' $k = {o1.k:.1f},$' + \
+                rf' $l = {o1.ang_mom:.1f},$' + \
+                rf' $r_0 = {r_0},$' + \
+                rf' $\dot r_0 = {r_dot_0},$' + \
+                rf' $\phi_0 = {phi_0}$' + \
+                '\n'     # \n means a new line (adds some space here)
+fig_4.suptitle(overall_title, va='baseline')
+
+ax_4d = fig_4.add_subplot(1,1,1, polar=True)
+ax_4d.plot(phi_pts_l[0,:], r_pts_l[0,:], color='blue', label = "Leapfrog")
+ax_4d.plot(phi_pts_l[1,:], r_pts_l[1,:], color='red', label = "Leapfrog")
+#ax_4d.plot(phi_pts_l[2,:], r_pts_l[2,:], color='blue', label = "Leapfrog")
+ax_4d.set_title('Polar plot', pad=20.)
+#ax_4d.set_ylim(0,4)
+
+fig_4.tight_layout()
+
+fig_4.savefig("Orbits_plots_polar.png", bbox_inches = "tight")
